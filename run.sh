f84b530fc9a4277aa5fb35e96dfd581925c3485d
Gilbert <gdpoke@hanmail.net>
2022-07-20 16:25:12 +0900

    f

--- run.sh
+++ run.sh
@@ -8,6 +8,6 @@ if [ ! -s "$PID_FILE" ] || ! kill -0 $(cat $PID_FILE) > /dev/null 2>&1; then
   echo $$ > "$PID_FILE"
   lsof -i :8890 | grep 'python' | awk '{print $2}' | xargs
   sleep 3
-  nohub ./venv/bin/python -m apps.app >> "$APP_DIR/gr.log"
+  nohup ./venv/bin/python -m apps.app >> "$APP_DIR/gr.log"
 fi
 popd

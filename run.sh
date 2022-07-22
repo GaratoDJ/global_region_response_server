bc14154ec4eaa55faa8426863e23db1b8ff873a3
Gilbert <gdpoke@hanmail.net>
2022-07-22 18:19:36 +0900

    f

--- run.sh
+++ run.sh
@@ -3,11 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 APP_DIR="$DIR/"
 PID_FILE="$APP_DIR/gr.pid"
 
-pushd "$APP_DIR"
-if [ ! -s "$PID_FILE" ] || ! kill -0 $(cat $PID_FILE) > /dev/null 2>&1; then
-  echo $$ > "$PID_FILE"
-  lsof -i :8890 | grep 'python' | awk '{print $2}' | xargs
-  sleep 3
-  nohup ./venv/bin/python -m apps.app >> "$APP_DIR/gr.log"
-fi
-popd
+echo $$ > "$PID_FILE"
+lsof -i :8890 | grep 'ython' | awk '{print $2}' | xargs kill -9
+sleep 3
+nohup ./venv/bin/python -m apps.app >> "$APP_DIR/gr.log" &
